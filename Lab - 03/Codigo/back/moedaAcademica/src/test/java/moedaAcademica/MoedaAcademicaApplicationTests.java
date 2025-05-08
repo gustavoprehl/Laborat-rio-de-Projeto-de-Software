d45956cbdc874e8d8cbbac87e89912bd1b1faedd
guilherme_silveira <91088737+guilhermsilveira@users.noreply.github.com>
2025-05-07 22:11:46 -0300

    Estrutura back e conexao banco de dados

---
+++ Lab - 03/Codigo/back/moedaAcademica/src/test/java/moedaAcademica/MoedaAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package moedaAcademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoedaAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
