c8eb78a55a80efdce68c94b4b0178036ab2bb363
Guilhermeh-R <rodriguesguilerme4@gmail.com>
2025-05-08 17:40:59 -0300

    Adicionando moeda

--- Lab - 03/Codigo/back/moedaAcademica/src/main/java/moedaAcademica/api/controller/MoedaController.java
+++ Lab - 03/Codigo/back/moedaAcademica/src/main/java/moedaAcademica/api/controller/MoedaController.java
@@ -1,27 +1,37 @@
 package moedaAcademica.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import moedaAcademica.application.usecases.GerenciarTransações;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
-
 @RestController
-@RequestMapping("/moeda")
+@RequestMapping("/moedas")
 public class MoedaController {
-
     @Autowired
-    private GerenciarTransações gerenciarTransações;
+    private GerenciarTransações gerenciarTransacoes;
 
-    @RequestMapping(value = "/requestMethodName", method = RequestMethod.GET)
-    public String requestMethodName(@RequestParam String param) {
-        return new String();
+    public MoedaController(GerenciarTransações gerenciarTransacoes) {
+        this.gerenciarTransacoes = gerenciarTransacoes;
     }
-    
+@PostMapping("/transferir")
+public String transferirMoeda(int remetenteId, int destinatarioId, int quantidade) {
+    boolean sucesso = gerenciarTransacoes.transferirMoeda(remetenteId, destinatarioId, quantidade);
+    if (sucesso) {
+        return "Transferência realizada com sucesso!";
+    } else {
+        return "Saldo insuficiente ou usuário não encontrado.";
+    }
+}
+@PostMapping("/adicionar")
+public String adicionarMoeda(int usuarioId, int quantidade) {
+    boolean sucesso = gerenciarTransacoes.adicionarMoeda(usuarioId, quantidade);
+    if (sucesso) {
+        return "Moeda adicionada com sucesso!";
+    } else {
+        return "Usuário não encontrado.";
+    }
+}
+
 }
